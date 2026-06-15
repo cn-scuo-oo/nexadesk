a789292e8ade003bc4031a5ea4964fd51353bb25
cn-scuo-oo <493720003@qq.com>
2026-06-15 19:09:12 +0800

    refactor(web): App.tsx split guide + re-export barrel

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,8 @@
+// Components barrel re-export
+export { default as ProviderStatusPanel } from "./ProviderStatusPanel";
+export { default as ProviderConfigPanel } from "./ProviderConfigPanel";
+export { default as IMSettingsPanel } from "./IMSettingsPanel";
+export { default as EmailConfigPanel } from "./EmailConfigPanel";
+export { SettingsModal } from "./modals/SettingsModal";
+export { AgentEditorModal } from "./modals/AgentEditorModal";
+export { McpServerEditorModal } from "./modals/McpServerEditorModal";
