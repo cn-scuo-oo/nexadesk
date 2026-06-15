56d4e5c333fead025c1f9dd4f9d94b26deda7f39
cn-scuo-oo <493720003@qq.com>
2026-06-15 19:09:10 +0800

    refactor(web): App.tsx split guide + re-export barrel

---
+++ apps/web/src/views/index.ts
@@ -0,0 +1,11 @@
+// View components barrel re-export
+export { default as NewTaskView } from "./NewTaskView";
+export { default as TaskThreadView } from "./TaskThreadView";
+export { default as TaskRunPanel } from "./TaskRunPanel";
+export { default as TaskSearchView } from "./TaskSearchView";
+export { default as ScheduledTasksView } from "./ScheduledTasksView";
+export { default as RuntimeDashboardView } from "./RuntimeDashboardView";
+export { default as McpHubView } from "./McpHubView";
+export { default as MemoryHubView } from "./MemoryHubView";
+export { default as AgentsHubView } from "./AgentsHubView";
+export { default as SettingsCenter } from "./SettingsCenter";
